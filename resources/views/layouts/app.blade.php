ba80f81bc3af2e923b92a766b546130d5d74e4e1
Rotoros <gonzalo.poyatos@insbaixcamp.cat>
2025-10-30 10:49:33 +0100

    Colores

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,16 @@
     <title>Gestor de Libros</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="p-4">
-    <div class="container">
+<body style="background-color: #f2f4f8; min-height: 100vh;">
+    <div class="container py-4">
         <nav class="mb-4">
             <a href="{{ route('books.index') }}" class="btn btn-outline-primary">Libros</a>
             <a href="{{ route('authors.index') }}" class="btn btn-outline-secondary">Autores</a>
         </nav>
-        @yield('content')
+
+        <div class="card shadow p-4 rounded-4 bg-white">
+            @yield('content')
+        </div>
     </div>
 </body>
 </html>
